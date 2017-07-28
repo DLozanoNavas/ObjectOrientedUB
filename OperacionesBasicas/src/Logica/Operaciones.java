60c5bfeda4044652fd10df6c29984f0251f9a687
Daniel Fernando Lozano <DLozanoNavas@users.noreply.github.com>
2017-07-28 10:59:46 -0500

    28/07/17

---
+++ OperacionesBasicas/src/Logica/Operaciones.java
@@ -0,0 +1,82 @@
+package Logica;
+
+
+// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+// #[regen=yes,id=DCE.B978B282-B1EC-FFC8-5225-966B0F9464BE]
+// </editor-fold> 
+public class Operaciones {
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.CCF69B51-B5F6-D6EE-C9E8-38A5938FA7EA]
+    // </editor-fold> 
+    private int num1;
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.B1F1C6D3-DF44-CB3E-00C1-EB0FE4A47772]
+    // </editor-fold> 
+    private int num2;
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.05A3A58C-14A7-81C2-C592-CBE432EBD9BD]
+    // </editor-fold> 
+    public Operaciones () {
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,regenBody=yes,id=DCE.194CB72B-3A16-806C-05D5-FD4818BA153E]
+    // </editor-fold> 
+    public int getNum1 () {
+        return num1;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,regenBody=yes,id=DCE.36620148-9C41-5ADE-796B-B2AC78246366]
+    // </editor-fold> 
+    public void setNum1 (int val) {
+        this.num1 = val;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,regenBody=yes,id=DCE.6EDD4D84-137C-9301-1108-4E47DDC5579A]
+    // </editor-fold> 
+    public int getNum2 () {
+        return num2;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,regenBody=yes,id=DCE.2C7E4EB5-44DF-045C-5143-E845DC4F80CC]
+    // </editor-fold> 
+    public void setNum2 (int val) {
+        this.num2 = val;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.7889F76F-A7CC-448E-CCAA-764846DD0868]
+    // </editor-fold> 
+    public int suma () {
+        return 0;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.BD1B3ADD-743A-53FF-A68D-B947208ADE67]
+    // </editor-fold> 
+    public int resta () {
+        return 0;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.2214CF44-FE84-9E75-F5CC-475C9FEA4FE8]
+    // </editor-fold> 
+    public int multiplicacion () {
+        return 0;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.18BF75FE-7FEF-42ED-8A4B-C718B950C4FF]
+    // </editor-fold> 
+    public int division () {
+        return 0;
+    }
+
+}
+
---
+++ OperacionesBasicas/src/Principal/Principal.java
@@ -0,0 +1,16 @@
+package Principal;
+
+
+// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+// #[regen=yes,id=DCE.589CB90E-CE96-1A7A-B369-6B676E922304]
+// </editor-fold> 
+public class Principal {
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.9DF7F567-F49E-6122-D37D-AA8D04EC8D4D]
+    // </editor-fold> 
+    public Principal () {
+    }
+
+}
+
